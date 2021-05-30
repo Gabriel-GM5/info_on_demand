7dbd9349306ad5931ab0620bd556e405c0b3ede8
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-05-30 19:43:07 -0300

    Ajuste pequeno

--- application/libraries/Custom.php
+++ application/libraries/Custom.php
@@ -4,14 +4,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Custom
 {
-	public function renderizarPagina($view = false, $dados = false)
+	public function renderizarPagina($view = false, $data = false)
 	{
 		if ($view) {
 			$CI = &get_instance();
 			$CI->load->view('includes/header');
 			$CI->load->view('includes/navbar');
 			$CI->load->view('includes/content-start');
-			$CI->load->view($view);
+			$CI->load->view($view, $data);
 			$CI->load->view('includes/content-end');
 			$CI->load->view('includes/footer');
 		}
