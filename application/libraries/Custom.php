492b0be823d4ad242d85ff0961986cd92c2ee898
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-05-30 19:31:39 -0300

    Fix

--- application/libraries/Custom.php
+++ application/libraries/Custom.php
@@ -4,23 +4,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Custom
 {
-	var $contr;
 	public function __construct($params = array())
 	{
-		$this->ci = &get_instance();
-		$this->ci->load->helper('url');
-		$this->controller = $this->ci->uri->segment(2);
+		$CI = &get_instance();
 	}
 
 	public function renderizarPagina($view = false, $dados = false)
 	{
 		if ($view) {
-			$this->load->view('includes/header');
-			$this->load->view('includes/navbar');
-			$this->load->view('includes/content-start');
-			$this->load->view('includes/' . $view);
-			$this->load->view('includes/content-end');
-			$this->load->view('includes/footer');
+			$this->CI->view('includes/header');
+			$this->CI->view('includes/navbar');
+			$this->CI->view('includes/content-start');
+			$this->CI->view('includes/' . $view);
+			$this->CI->view('includes/content-end');
+			$this->CI->view('includes/footer');
 		}
 	}
 }
