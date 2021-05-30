ba78d1102da58aa1d2926dcaacee3e9667b4c7ac
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-05-30 19:34:58 -0300

    Ajuste

--- application/libraries/Custom.php
+++ application/libraries/Custom.php
@@ -4,16 +4,20 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Custom
 {
-	public function renderizarPagina($view = false, $dados = false)
+	public function __construct($params = array())
 	{
 		$CI = &get_instance();
+	}
+
+	public function renderizarPagina($view = false, $dados = false)
+	{
 		if ($view) {
-			$this->CI->view('includes/header');
-			$this->CI->view('includes/navbar');
-			$this->CI->view('includes/content-start');
-			$this->CI->view('includes/' . $view);
-			$this->CI->view('includes/content-end');
-			$this->CI->view('includes/footer');
+			$this->CI->load->view('includes/header');
+			$this->CI->load->view('includes/navbar');
+			$this->CI->load->view('includes/content-start');
+			$this->CI->load->view('includes/' . $view);
+			$this->CI->load->view('includes/content-end');
+			$this->CI->load->view('includes/footer');
 		}
 	}
 }
