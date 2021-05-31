189a94354279574934128efd77193a2b5cd2ae14
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-05-30 21:41:27 -0300

    favicon

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
 	<title>IOD</title>
+	<link rel="shortcut icon" type="image/jpg" href="<?php echo base_url('assets/icons/Favicon.svg') ?>"/>
 
 	<!-- CSS  -->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
