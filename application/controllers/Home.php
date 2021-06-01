d1328c0e89c544e831b8b8ffa2d78e43a416ba50
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 18:37:45 -0300

    login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,10 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-
-	public function index()
+class Home extends CI_Controller
+{
+	public function index($login = false)
 	{
-		$this->custom->renderizarPagina('home/teste');
+		if ($this->ion_auth->logged_in()) {
+			redirect('home/dashboard', 'refresh');
+		} else {
+			if ($login) {
+				redirect('home/login', 'refresh');
+			} else {
+				redirect('home/landing-page', 'refresh');
+			}
+		}
+	}
+
+	public function login()
+	{
+		if (!$this->ion_auth->logged_in()) {
+			$this->custom->renderizarPagina('home/login');
+		} else {
+			redirect('home', 'refresh');
+		}
 	}
 }
