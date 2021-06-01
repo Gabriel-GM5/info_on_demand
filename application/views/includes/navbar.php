dbf89c210c4c54b7f3ab0cc3d08112eeed8b38f8
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 19:26:37 -0300

    logo on navbar

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -1,3 +1,11 @@
-<nav>
-
-</nav>
+<div class="navbar-fixed">
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#!" class="brand-logo"><img src="<?php echo base_url('assets/icons/Logo.svg') ?>"></a>
+			<ul class="right hide-on-med-and-down">
+				<li><a href="sass.html">Sass</a></li>
+				<li><a href="badges.html">Components</a></li>
+			</ul>
+		</div>
+	</nav>
+</div>
