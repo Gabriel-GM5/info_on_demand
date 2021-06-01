74fcc53eae72ddbc46e015ead0b9492bcbcc2fe4
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 20:05:34 -0300

    Teste

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -1,20 +1,23 @@
+<?php
+$menu = '
+<li><a href="sass.html">Sass</a></li>
+<li><a href="badges.html">Components</a></li>
+<li><a href="collapsible.html">Javascript</a></li>
+<li><a href="mobile.html">Mobile</a></li>
+';
+?>
+
 <div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
 			<a href="<?php echo site_url() ?>" class="brand-logo"><img src="<?php echo base_url('assets/icons/Logo.svg') ?>" width="auto" height="56"></a>
 			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 			<ul class="right hide-on-med-and-down">
-				<li><a href="sass.html">Sass</a></li>
-				<li><a href="badges.html">Components</a></li>
-				<li><a href="collapsible.html">Javascript</a></li>
-				<li><a href="mobile.html">Mobile</a></li>
+				<?php echo $menu ?>
 			</ul>
 		</div>
 	</nav>
 	<ul class="sidenav" id="mobile-demo">
-		<li><a href="sass.html">Sass</a></li>
-		<li><a href="badges.html">Components</a></li>
-		<li><a href="collapsible.html">Javascript</a></li>
-		<li><a href="mobile.html">Mobile</a></li>
+		<?php echo $menu; ?>
 	</ul>
 </div>
