97fdadb3d17128bf0bce545ddd49e85407255920
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 19:31:32 -0300

    teste

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -1,7 +1,7 @@
 <div class="navbar-fixed">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#!" class="brand-logo"><img src="<?php echo base_url('assets/icons/Logo.svg') ?>" max-widith="150" height="auto"></a>
+			<a href="#!" class="brand-logo"><img src="<?php echo base_url('assets/icons/Logo.svg') ?>" widith="150" height="auto"></a>
 			<ul class="right hide-on-med-and-down">
 				<li><a href="sass.html">Sass</a></li>
 				<li><a href="badges.html">Components</a></li>
