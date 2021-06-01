83d737d238d84a192def382df4f98006cbd24154
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 19:44:37 -0300

    navbar

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -2,10 +2,19 @@
 	<nav>
 		<div class="nav-wrapper">
 			<a href="<?php echo site_url() ?>" class="brand-logo"><img src="<?php echo base_url('assets/icons/Logo.svg') ?>" width="auto" height="56"></a>
+			<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 			<ul class="right hide-on-med-and-down">
 				<li><a href="sass.html">Sass</a></li>
 				<li><a href="badges.html">Components</a></li>
+				<li><a href="collapsible.html">Javascript</a></li>
+				<li><a href="mobile.html">Mobile</a></li>
 			</ul>
 		</div>
 	</nav>
+	<ul class="sidenav" id="mobile-demo">
+		<li><a href="sass.html">Sass</a></li>
+		<li><a href="badges.html">Components</a></li>
+		<li><a href="collapsible.html">Javascript</a></li>
+		<li><a href="mobile.html">Mobile</a></li>
+	</ul>
 </div>
