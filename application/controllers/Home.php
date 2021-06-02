892b0b401d71309afa5997335c678cbc5cc6a522
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 21:12:59 -0300

    Login

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -19,6 +19,7 @@ class Home extends CI_Controller
 	public function login()
 	{
 		if (!$this->ion_auth->logged_in()) {
+			$this->load->helper('form');
 			$this->custom->renderizarPagina('home/login');
 		} else {
 			redirect('home', 'refresh');
