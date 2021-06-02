3e8da66a17d790a8a25b5b531e38dd62efa9300f
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 23:16:10 -0300

    Ajuste

--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -8,6 +8,20 @@ if ($logado) {
     		' . $nomeUsuario . '
   		</div>
   	</li>';
+?>
+	<div id="modalUsuario" class="modal">
+		<div class="modal-content">
+			<h4><?php echo $nomeUsuario . $sobrenomeUsuario ?></h4>
+			<div class="collection">
+				<a href="#!" class="collection-item">Editar</a>
+				<a href="<?php echo site_url('home/logout') ?>" class="collection-item">Sair</a>
+			</div>
+		</div>
+		<div class="modal-footer">
+			<a href="#!" class="modal-close btn-flat">Voltar</a>
+		</div>
+	</div>
+<?php
 } else {
 	$menu .= '<li><a class="btn botaoNavBar" href="' . site_url('home/login') . '">Login</a></li>';
 }
@@ -35,19 +49,6 @@ $menu .= '
 			</ul>
 		</div>
 	</nav>
-</div>
-
-<div id="modalUsuario" class="modal">
-	<div class="modal-content">
-		<h4><?php echo $nomeUsuario . $sobrenomeUsuario ?></h4>
-		<div class="collection">
-			<a href="#!" class="collection-item">Editar</a>
-			<a href="<?php echo site_url('home/logout') ?>" class="collection-item">Sair</a>
-		</div>
-	</div>
-	<div class="modal-footer">
-		<a href="#!" class="modal-close btn-flat">Voltar</a>
-	</div>
 </div>
 
 <ul class="sidenav" id="mobile-demo">
