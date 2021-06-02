4f239a21ea6989db7d4be80a748fd39837105c79
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 22:36:38 -0300

    Ajustes

--- application/libraries/Custom.php
+++ application/libraries/Custom.php
@@ -8,8 +8,9 @@ class Custom
 	{
 		$CI = &get_instance();
 		if ($view) {
-			$user = $this->ion_auth->user()->row();
-			$dadosNav = array('logado' => $CI->ion_auth->logged_in(), 'nomeUsuario' => $user->first_name, 'idUsuario' => $user->id);
+			$logged_in = $CI->ion_auth->logged_in();
+			$user = $CI->ion_auth->user()->row();
+			$dadosNav = array('logado' => $logged_in, 'nomeUsuario' => $user->first_name, 'idUsuario' => $user->id);
 			$CI->load->view('includes/header');
 			$CI->load->view('includes/navbar', $dadosNav);
 			$CI->load->view('includes/content-start');
