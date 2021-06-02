ab8dcf9a1e7de71413e947537a55081f5d463268
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-01 21:23:06 -0300

    Teste

--- application/views/home/login.php
+++ application/views/home/login.php
@@ -4,7 +4,7 @@
 	</div>
 	<div class='col s12 m6'>
 		<div class='row'>
-			<div class='col s12 m8 l6 offset-m2 offset-l3'>
+			<div class='col s12 l8 offset-l2'>
 				<div class="card">
 					<div class="card-content">
 						<span class="card-title">Login</span>
