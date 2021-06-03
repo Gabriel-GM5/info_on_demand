8ec22bb21b2eb2799f1ed9118f74492ffeccc826
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-03 00:07:42 -0300

    Ajuste

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,11 +1,3 @@
-<footer class="page-footer">
-	<div class="footer-copyright">
-		<div class="container">
-			© 2014 Copyright Text
-			<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-		</div>
-	</div>
-</footer>
 <script src="<?php echo base_url('assets/js/jquery-2.1.1.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/materialize.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
