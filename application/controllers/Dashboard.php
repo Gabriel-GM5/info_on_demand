d9cb11666d6388c9e5a3611f4b8e31fb3d2d10cb
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-03 20:55:45 -0300

    Teste

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,6 +8,19 @@ class Dashboard extends CI_Controller
 		if ($this->ion_auth->in_group(2)) {
 			$this->load->helper('form');
 			$this->custom->renderizarPagina('dashboard/dashboard');
+		} else {
+			redirect('home', 'refresh');
+		}
+	}
+
+	public function novaPostagem()
+	{
+		if ($this->ion_auth->in_group(2)) {
+			var_dump($_POST);
+			var_dump($_FILES);
+			exit;
+		} else {
+			redirect('home', 'refresh');
 		}
 	}
 }
