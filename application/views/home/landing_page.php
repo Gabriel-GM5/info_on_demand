1dfce306bb7cd7b9e2e07cb46f9385c55d3de4ce
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-03 15:43:46 -0300

    Teste

--- application/views/home/landing_page.php
+++ application/views/home/landing_page.php
@@ -1,8 +1,8 @@
-<div>
-	<div>
-		<div>
+<div class="container">
+	<div class="row">
+		<div class="col s12">
 			<form>
-				<div class="input-field">
+				<div class="input-field col s12">
 					<input id="search" type="search" required>
 					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
 					<i class="material-icons">close</i>
