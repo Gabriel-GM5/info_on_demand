ad35e436ed32e7d00c1e7d4b5bf34384cc6536df
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-03 15:47:09 -0300

    Teste

--- application/views/home/landing_page.php
+++ application/views/home/landing_page.php
@@ -1,13 +1,24 @@
 <div class="container">
 	<div class="row">
-		<div class="col s12">
-			<form>
-				<div class="input-field col s12">
-					<input id="search" type="search" required>
-					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
-					<i class="material-icons">close</i>
+		<div class='col s12 m8 l6 offset-m2 offset-l3'>
+			<div class="card">
+				<div class="card-content">
+					<?php
+					echo form_open();
+					?>
+					<div class="row">
+						<div class="input-field col s12">
+							<?php
+							echo form_input(array('id' => "search", 'name' => "search", 'type' => "text", 'class' => "validate", 'required' => "required"));
+							echo form_label('Pesquisar', 'search');
+							?>
+						</div>
+					</div>
+					<?php
+					echo form_close();
+					?>
 				</div>
-			</form>
+			</div>
 		</div>
 	</div>
 </div>
