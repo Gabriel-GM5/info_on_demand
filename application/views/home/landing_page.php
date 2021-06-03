dee44d4452ceeac8475f81a0e8100ec1bafa940e
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-03 15:47:45 -0300

    Ajuste 2

--- application/views/home/landing_page.php
+++ application/views/home/landing_page.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="row">
-		<div class='col s12 m8 l6 offset-m2 offset-l3'>
+		<div class='col s12 m8 offset-m2'>
 			<div class="card">
 				<div class="card-content">
 					<?php
