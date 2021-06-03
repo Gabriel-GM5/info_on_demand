0bda42a0a55b76e0e5da3452a187a67deafe9f04
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-03 15:34:59 -0300

    Teste

---
+++ application/views/home/landing_page.php
@@ -0,0 +1,11 @@
+<script src="//cdn.ckeditor.com/4.16.1/standard/ckeditor.js"></script>
+<form>
+            <textarea name="editor1" id="editor1" rows="10" cols="80">
+                This is my textarea to be replaced with CKEditor 4.
+            </textarea>
+            <script>
+                // Replace the <textarea id="editor1"> with a CKEditor 4
+                // instance, using default configuration.
+                CKEDITOR.replace( 'editor1' );
+            </script>
+</form>
