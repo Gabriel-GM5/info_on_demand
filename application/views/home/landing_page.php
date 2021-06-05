decbedd2798d27b14e244973491a7348cbaf2f21
Gabriel-GM5 <gabrielquintoand@gmail.com>
2021-06-04 22:32:53 -0300

    Ajustes

--- application/views/home/landing_page.php
+++ application/views/home/landing_page.php
@@ -40,42 +40,6 @@ if ($posts) {
 		</div>
 		<div class="row">
 			<div class="col s12 m6">
-				<h6>Posts</h6>
-				<ul class="collection">
-					<?php
-					foreach ($posts as $post) {
-					?>
-						<li class="collection-item avatar">
-							<?php
-							if (isset($post->imagem)) {
-							?>
-								<img src="data:image/*;base64,<?php echo base64_encode($post->imagem) ?>" alt="" class="circle">
-							<?php
-							} else {
-							?>
-								<img src="<?php echo base_url('assets/images/sem_miniatura.png') ?>" alt="" class="circle">
-							<?php
-							}
-							?>
-							<a href="<?php echo site_url('posts/ver/' . $post->idPost) ?>" target="_blank"><span class="title"><?php echo $post->titulo ?></span>
-								<p>
-									<?php
-									if ($post->subtitulo) {
-										echo $post->subtitulo;
-									}
-									?>
-									<br>
-									Por <strong><?php echo $post->nomeUsuario ?>&nbsp;<?php echo $post->sobrenomeUsuario ?></strong>
-									<br>
-								</p>
-							</a>
-						</li>
-					<?php
-					}
-					?>
-				</ul>
-			</div>
-			<div class="col s12 m6">
 				<h6>Imagens</h6>
 				<ul class="collection">
 					<?php
@@ -130,6 +94,42 @@ if ($posts) {
 							</li>
 					<?php
 						}
+					}
+					?>
+				</ul>
+			</div>
+			<div class="col s12">
+				<h6>Posts</h6>
+				<ul class="collection">
+					<?php
+					foreach ($posts as $post) {
+					?>
+						<li class="collection-item avatar">
+							<?php
+							if (isset($post->imagem)) {
+							?>
+								<img src="data:image/*;base64,<?php echo base64_encode($post->imagem) ?>" alt="" class="circle">
+							<?php
+							} else {
+							?>
+								<img src="<?php echo base_url('assets/images/sem_miniatura.png') ?>" alt="" class="circle">
+							<?php
+							}
+							?>
+							<a href="<?php echo site_url('posts/ver/' . $post->idPost) ?>" target="_blank"><span class="title"><?php echo $post->titulo ?></span>
+								<p>
+									<?php
+									if ($post->subtitulo) {
+										echo $post->subtitulo;
+									}
+									?>
+									<br>
+									Por <strong><?php echo $post->nomeUsuario ?>&nbsp;<?php echo $post->sobrenomeUsuario ?></strong>
+									<br>
+								</p>
+							</a>
+						</li>
+					<?php
 					}
 					?>
 				</ul>
